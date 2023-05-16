0e2df6c10d4cd0f9d7372fefd657bb80f5f2b1ad
Joana Leoni <joanaleonipereira@gmail.com>
2023-05-16 19:41:12 -0300

    Código fonte

--- Exercicios/07-led-rgb/codigo-fonte.cpp
+++ Exercicios/07-led-rgb/codigo-fonte.cpp
@@ -2,8 +2,10 @@ void setup()
 {
   //Porta 11 em modo de saída
   pinMode(11, OUTPUT); // Vermelho
+  
   //Porta 10 em modo de saída
   pinMode(10, OUTPUT); // Azul
+  
   //Porta 9 em modo de saída
   pinMode(9, OUTPUT); // Verde
 }
@@ -12,22 +14,21 @@ void loop()
 {
   // Escreve 255 no pino digital 11
   digitalWrite(11, 255);
-  // Aguarda 1000 milésimos de segundo
   delay(1000);
+  
   //Escreve 0 no pino digital 11
   digitalWrite(11, 0);
-  // Aguarda 1000 milésimos de segundo
   delay(1000);
+  
   // Escreve 255 no pino digital 10
   digitalWrite(10, 255);
-  // Aguarda 1000 milésimos de segundo
   delay(1000);
+  
   //Escreve 0 no pino digital 10
   digitalWrite(10, 0);
-  // Aguarda 1000 milésimos de segundo
   delay(1000);
+  
   // Escreve 255 no pino digital 9
   digitalWrite(9, 255);
-  // Aguarda 1000 milésimos de segundo
   delay(1000);
 }
